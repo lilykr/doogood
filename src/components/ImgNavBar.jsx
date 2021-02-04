321a6ed8c4aa63f1f1ffa2cab20be6e75035c746
lilykr <lisa-lou.k@hotmail.fr>
2021-02-04 21:08:54 +0100

    fixed navbar

--- src/components/ImgNavBar.jsx
+++ src/components/ImgNavBar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-function ImgNavBar({isActive, onClick, Image, ImageActive, alt}) {
+function ImgNavBar({isActive, Image, ImageActive, alt}) {
     return (
         <div>
-            {isActive === alt ?
-                <img onClick={() => onClick(alt)} width="30%" src={ImageActive} alt={alt} />
-                : <img onClick={() => onClick(alt)} width="30%" src={Image} alt={alt} />
+            {isActive  ?
+                <img  width="30%" src={ImageActive} alt={alt} />
+                : <img  width="30%" src={Image} alt={alt} />
             }
         </div>
     )
@@ -13,4 +13,6 @@ function ImgNavBar({isActive, onClick, Image, ImageActive, alt}) {
 }
 
 export default ImgNavBar;
+
+
 
